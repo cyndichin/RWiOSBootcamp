70ad2422a6fbc6efef90a3b453f8a4fe5a5891a7
Cyndi Chin <cgc95@cornell.edu>
2020-05-31 17:19:29 -0400

    Add Stretch Challenge (HSB)

--- Week01/Week01Homework/ColorSlider/ColorSlider/ViewController.swift
+++ Week01/Week01Homework/ColorSlider/ColorSlider/ViewController.swift
@@ -14,33 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var redValueLabel: UILabel!
     @IBOutlet weak var greenValueLabel: UILabel!
     @IBOutlet weak var blueValueLabel: UILabel!
+    @IBOutlet weak var firstTitleLabel: UILabel!
+    @IBOutlet weak var secondTitleLabel: UILabel!
+    @IBOutlet weak var thirdTitleLabel: UILabel!
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var backgroundView: UIView!
-    @IBOutlet weak var rgbView: UIView!
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    var segmentIndex: Int {
+        return segmentControl.selectedSegmentIndex
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        super.viewWillTransition(to: size, with: coordinator)
-        let screenHeight = UIScreen.main.bounds.size.height
-        let baseScreenHeight = UIScreen.main.bounds.height //Layout is iphone 8 plus and 128(redViewheight) according to this.
-        let heightRatio = UIScreen.main.bounds.height/UIScreen.main.bounds.width
-        coordinator.animate(alongsideTransition: { context in
-            //This function will do the trick
-            self.rgbView.translatesAutoresizingMaskIntoConstraints = false
-            let orientation = UIDevice.current.orientation
-            guard orientation != .portrait else { return }
-            self.rgbView.heightAnchor.constraint(equalToConstant: baseScreenHeight * heightRatio).isActive = true
-            self.rgbView.widthAnchor.constraint(equalToConstant: baseScreenHeight * heightRatio).isActive = true
-           
-            self.view.layoutIfNeeded()
-
-        })
     }
 
     @IBAction func setColor() {
@@ -51,7 +41,7 @@ class ViewController: UIViewController {
         }
         let action = UIAlertAction(title: "Enter", style: .default) { alert in
             let textField = alertVC.textFields![0]
-            self.updateColorName(with: textField.text ?? "Color Name")
+            self.updateColor(with: textField.text ?? "Color Name")
         }
         alertVC.addAction(action)
         present(alertVC, animated: true, completion: nil)
@@ -71,27 +61,62 @@ class ViewController: UIViewController {
         colorLabel.text = "0"
     }
     
-//    @IBAction func sliderMoved(_ slider: UISlider) {
-//        redValueLabel.text = String(Int(redSlider.value))
-//        greenValueLabel.text = String(Int(greenSlider.value))
-//        blueValueLabel.text = String(Int(blueSlider.value))
-//    }
-    
-    @IBAction func changeColor(_ sender: AnyObject) {
-        let red = redSlider.value
-        let green = greenSlider.value
-        let blue = blueSlider.value
-        colorNameLabel.textColor = UIColor(red: CGFloat((255 - red)/255), green: CGFloat((255 - green)/255), blue: CGFloat((255 - blue)/255), alpha: 1.0)
+    @IBAction func sliderMoved(_ sender: AnyObject) {
+        let red = redSlider.value.rounded()
+        let green = greenSlider.value.rounded()
+        let blue = blueSlider.value.rounded()
+        let previewColor: UIColor
+        if segmentIndex == 0 {
+            previewColor = UIColor(red: CGFloat(red/255.0), green: CGFloat(green/255.0), blue: CGFloat(blue/255.0), alpha: 1.0)
+        } else {
+            previewColor = UIColor(hue: CGFloat(red/360.0), saturation: CGFloat(green/100.0), brightness: CGFloat(blue/100.0), alpha: 1.0)
+        }
         redValueLabel.text = String(Int(red))
         greenValueLabel.text = String(Int(green))
         blueValueLabel.text = String(Int(blue))
-        print("Red \(red) Green \(green) Blue \(blue)")
-        print("Red \(CGFloat(red)) Green \(CGFloat(green)) Blue \(CGFloat(blue))")
-        backgroundView.backgroundColor = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+        previewView.backgroundColor = previewColor
     }
     
-    func updateColorName(with name: String) {
+    func updateColor(with name: String) {
         colorNameLabel.text = name
+        let red = redSlider.value.rounded()
+        let green = greenSlider.value.rounded()
+        let blue = blueSlider.value.rounded()
+        let nameColor: UIColor
+        let bgColor: UIColor
+        if segmentIndex == 0 {
+            nameColor = UIColor(red: CGFloat((255 - red)/255), green: CGFloat((255 - green)/255), blue: CGFloat((255 - blue)/255), alpha: 1.0)
+            bgColor = UIColor(red: CGFloat(red/255.0), green: CGFloat(green/255.0), blue: CGFloat(blue/255.0), alpha: 1.0)
+        } else {
+            nameColor = UIColor(hue: CGFloat((360 - red)/360), saturation: CGFloat((100 - green)/100), brightness: CGFloat((100 - blue)/100), alpha: 1.0)
+            bgColor = UIColor(hue: CGFloat(red/360.0), saturation: CGFloat(green/100.0), brightness: CGFloat(blue/100.0), alpha: 1.0)
+        }
+        colorNameLabel.textColor = nameColor
+        backgroundView.backgroundColor = bgColor
+    }
+    
+    @IBAction func modeChanged(_ sender: Any) {
+        let index = segmentControl.selectedSegmentIndex
+        if index == 0 {
+            setTitleLabels("Red", "Green", "Blue")
+            setSliderMaximum(255.0, 255.0, 255.0)
+        } else if index == 1 {
+            setTitleLabels("Hue", "Saturation", "Brightness")
+            setSliderMaximum(360.0, 100.0, 100.0)
+        }
+        reset()
+    }
+    
+    func setSliderMaximum(_ firstMax: Float, _ secondMax: Float, _ thirdMax: Float) {
+        redSlider.maximumValue = firstMax
+        greenSlider.maximumValue = secondMax
+        blueSlider.maximumValue = thirdMax
+    }
+    
+    func setTitleLabels(_ firstLabel: String, _ secondLabel: String, _ thirdLabel: String) {
+        firstTitleLabel.text = firstLabel
+        secondTitleLabel.text = secondLabel
+        thirdTitleLabel.text = thirdLabel
     }
 }
 
