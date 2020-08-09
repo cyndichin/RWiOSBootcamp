a0330ae72b75be7245c1df28e48a19885f4bea36
Cyndi Chin <cgc95@cornell.edu>
2020-08-09 12:44:08 -0400

    Finish Animating Buttons

--- Week11/Week11Homework/AnimationsHomeworl/AnimationsHomeworl/ViewController.swift
+++ Week11/Week11Homework/AnimationsHomeworl/AnimationsHomeworl/ViewController.swift
@@ -11,204 +11,89 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var animateObject: UIImageView!
-    @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var topButton: UIButton!
     @IBOutlet weak var leftButton: UIButton!
-    
+    @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var centerButton: UIButton!
+    
     private var isSettingAnimations = false
-    private var isTopTapped = false
-    private var isLeftTapped = false
-    private var isRightTapped = false
+    var animator: UIViewPropertyAnimator!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         animateObject.alpha = 0
-         hideSetting(with: leftButton)
-                    hideSetting(with: rightButton)
-                    hideSetting(with: topButton)
+        view.bringSubviewToFront(centerButton)
+        animator = UIViewPropertyAnimator(duration: 2, curve: .easeInOut)
     }
     
     @IBAction func playTapped(_ sender: Any) {
         isSettingAnimations.toggle()
         
         if isSettingAnimations {
-            // show settings
-            showSetting(with: leftButton, and: -(self.centerButton.frame.width + 20), false)
-            showSetting(with: rightButton, and: self.centerButton.frame.width + 20, false)
-            showSetting(with: topButton, and: self.centerButton.frame.height + 20, true)
-
-//            showItem()
+            showSetting(with: topButton, and: (x: 0, y: -(self.centerButton.frame.height + 20)))
+            showSetting(with: leftButton, and: (x: -(self.centerButton.frame.width + 20), y: 0))
+            showSetting(with: rightButton, and: (x: self.centerButton.frame.width + 20, y: 0))
         } else {
-            hideSetting(with: leftButton)
-            hideSetting(with: rightButton)
-            hideSetting(with: topButton)
-            // hides and play settings
-//            if isLeftTapped {
-//               showDragon()
-//            }
-//            
-//            if isTopTapped {
-//               upAndDown()
-//            }
-//            
-//            if isRightTapped {
-//               glowRed()
-//            }
-            
+            hideSettings()
+            animator.startAnimation()
         }
     }
     @IBAction func rightTapped(_ sender: Any) {
-        isRightTapped = true
+        animator.addAnimations {
+            UIView.animate(
+                withDuration: 1, delay: 0,
+                options: .curveEaseOut,
+                animations: { self.animateObject.layer.shadowColor = UIColor.red.cgColor
+                    self.animateObject.layer.shadowRadius = 5
+                    self.animateObject.layer.shadowOpacity = 1
+            }
+            )
+        }
     }
     
     @IBAction func leftTapped(_ sender: Any) {
-        isLeftTapped = true
+        animator.addAnimations {
+            self.animateObject.alpha = 1
+        }
     }
     
     @IBAction func topTapped(_ sender: Any) {
-        isTopTapped = true
-    }
-    
-    func showDragon() {
-        UIView.animate(
-            withDuration: 1, delay: 0,
-            options: .curveEaseOut,
-            animations: { self.animateObject.alpha = 1 }
-        )
-    }
-    
-    func upAndDown() {
-        UIView.animate(
-            withDuration: 1,
-            animations: { self.animateObject.frame.origin.y -= 50
-                
-        }){_ in
-            UIView.animateKeyframes(withDuration: 1, delay: 0, options: [.autoreverse, .repeat], animations: { self.animateObject.frame.origin.y += 50
-                
-            })
-        }
-    }
-    
-    func glowRed() {
-        UIView.animate(
-                   withDuration: 1, delay: 0,
-                   options: .curveEaseOut,
-                   animations: { self.animateObject.layer.shadowColor = UIColor.red.cgColor
-                    self.animateObject.layer.shadowRadius = 20
-        }
-               )
-    }
-    
-    func depart() {
-        let originalCenter = animateObject.center
-        UIView.animateKeyframes(
-            withDuration: 1.5, delay: 0,
-            animations: { [animateObject = self.animateObject!] in
-                // Move animateObject right & up
-                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
-                    animateObject.center.x += 80
-                    animateObject.center.y -= 10
-                }
-                
-                // Rotate animateObject
-                UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.4) {
-                    animateObject.transform = .init(rotationAngle: -.pi / 8)
-                }
-                
-                // Move animateObject right and up off screen, while fading out
-                UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
-                    animateObject.center.x += 100
-                    animateObject.center.y -= 50
-                    animateObject.alpha = 0
-                }
-                
-                // Move animateObject just off left side, reset transform and height
-                UIView.addKeyframe(withRelativeStartTime: 0.51, relativeDuration: 0.01) {
-                    animateObject.transform = .identity
-                    animateObject.center = .init(x: 0, y: originalCenter.y)
-                }
-                
-                //          // Move animateObject back to original position & fade in
-                //          UIView.addKeyframe(withRelativeStartTime: 0.55, relativeDuration: 0.45) {
-                //            animateObject.alpha = 1
-                //            animateObject.center = originalCenter
-                //          }
+        animator.addAnimations {
+            UIView.animate(
+                withDuration: 1,
+                animations: { self.animateObject.frame.origin.y -= 50
+                    
+            }){_ in
+                UIView.animateKeyframes(withDuration: 1, delay: 0, options: [.autoreverse, .repeat], animations: { self.animateObject.frame.origin.y += 50
+                    
+                })
             }
-        )
+        }
     }
     
-//    func showSettings() {
-//        UIView.animate(
-//            withDuration: 1,
-//            animations: { self.animateObject.frame.origin.y -= 50
-//
-//        }){_ in
-//            UIView.animateKeyframes(withDuration: 1, delay: 0, options: [.autoreverse, .repeat], animations: { self.animateObject.frame.origin.y += 50
-//
-//            })
-//        }
-//    }
-    
-    func showSetting(with settingView: UIButton, and distance: CGFloat, _ isTop: Bool) {
-            settingView.translatesAutoresizingMaskIntoConstraints = false
-          let widthConstraint =  settingView.centerXAnchor.constraint(equalTo: centerButton.centerXAnchor, constant: 0)
-        
-          let topConstraint =  settingView.topAnchor.constraint(equalTo: centerButton.topAnchor, constant: 0)
-          
-          NSLayoutConstraint.activate([
-            settingView.bottomAnchor.constraint(
-            equalTo: centerButton.bottomAnchor),
-            topConstraint,
-            widthConstraint,
-            settingView.heightAnchor.constraint(equalToConstant: 64.0),
-            settingView.widthAnchor.constraint(equalTo: settingView.heightAnchor),
-          ])
-
-          view.layoutIfNeeded()
-
+    func showSetting(with settingView: UIButton, and distance: (x: CGFloat, y: CGFloat)) {
         UIView.animate(
-            withDuration: 0.8, delay: 0,
-            usingSpringWithDamping: 0.6, initialSpringVelocity: 10,
+            withDuration: 0.5, delay: 0,
+            options: .allowUserInteraction,
             animations: {
-                if isTop {
-                    topConstraint.constant = distance
-                } else {
-                    widthConstraint.constant = distance
-                }
-              self.view.layoutIfNeeded()
-            }
-          )
+                settingView.transform = .init(translationX: distance.x, y: distance.y)
+                self.view.layoutIfNeeded()
         }
-    
-    
-    
-    func hideSetting(with settingView: UIButton) {
-        settingView.translatesAutoresizingMaskIntoConstraints = false
-         let widthConstraint =  settingView.centerXAnchor.constraint(equalTo: centerButton.centerXAnchor, constant: 0)
-              
-                let topConstraint =  settingView.topAnchor.constraint(equalTo: centerButton.topAnchor, constant: 0)
-                
-                NSLayoutConstraint.activate([
-                  settingView.bottomAnchor.constraint(
-                  equalTo: centerButton.bottomAnchor),
-                  topConstraint,
-                  widthConstraint,
-                  settingView.heightAnchor.constraint(equalToConstant: 64.0),
-                  settingView.widthAnchor.constraint(equalTo: settingView.heightAnchor),
-                ])
-        
-        UIView.animate(
-          withDuration: 0.8, delay: 0,
-          usingSpringWithDamping: 0.6, initialSpringVelocity: 10,
-          animations: {
-            self.view.layoutIfNeeded()
-          }
         )
     }
-    private func delay(seconds: TimeInterval, execute: @escaping () -> Void) {
-      DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: execute)
-    }
-
     
+    
+    func hideSettings() {
+        UIView.animate(
+            withDuration: 0.5, delay: 0,
+            options: .allowUserInteraction,
+            animations: {
+                self.topButton.transform = .init(translationX: 0, y: 0)
+                self.rightButton.transform = .init(translationX: 0, y: 0)
+                self.leftButton.transform = .init(translationX: 0, y: 0)
+                self.view.layoutIfNeeded()
+        }
+        )
+    }
 }
 
